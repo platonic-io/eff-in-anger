293c02e8e102f167471e8340dca43da37107b00f
Christopher Vogt <oss.nsp@cvogt.org>
2018-05-30 19:52:35 -0400

    allow store to fail

--- pet-store-backend/src/PetStore/Server.hs
+++ pet-store-backend/src/PetStore/Server.hs
@@ -15,14 +15,15 @@ module PetStore.Server where
 import           Control.Concurrent.MVar   (MVar, modifyMVar, newMVar)
 import           Control.Monad.Except      (ExceptT (..))
 import           Control.Monad.Freer       (Eff, Member, interpret, runM, send)
-import           Control.Monad.Freer.Error (Error, runError)
-import           Data.Aeson                (FromJSON, ToJSON, encode)
+import           Control.Monad.Freer.Error (Error, runError, throwError)
+import           Data.Aeson                (FromJSON, ToJSON, eitherDecode,
+                                            encode)
 import           Data.ByteString.Lazy      (ByteString)
 import qualified Data.Map                  as Map
 import           Data.Maybe                (fromMaybe)
 import           GHC.Generics              (Generic)
 import           Network.Wai.Handler.Warp  (run)
-import           Servant
+import           Servant                   hiding (throwError)
 -- /keep
 
 data Store = Store {
@@ -48,37 +49,43 @@ startServer port = do
 
     implementation = addPetHandler
 
-    addPetHandler :: (Member BusinessLogicEffect m) => Pet -> Eff m String
+    addPetHandler :: (Member BusinessLogicEffect m) => Pet -> Eff m [Pet]
     addPetHandler = send . AddPet
 
   run port application
 
-runBusinessLogic :: (Member StorageEffect m) => Eff (BusinessLogicEffect : m) x -> Eff m x
+runBusinessLogic :: (Member StorageEffect m, Member (Error ServantErr) m) => Eff (BusinessLogicEffect : m) x -> Eff m x
 runBusinessLogic = interpret eval where
-  eval :: (Member StorageEffect n) => BusinessLogicEffect a -> Eff n a
+  eval :: (Member StorageEffect n, Member (Error ServantErr) n) => BusinessLogicEffect a -> Eff n a
   eval (AddPet pet) = do
-    send $ AddDocument "pets" $ encode pet
-    pure $ "got: " ++ petName pet
+    pets <- (fmap (traverse eitherDecode)) <$> (send $ AddDocument "pets" $ encode pet)
+    case pets of
+      Right (Right p) -> pure p
+      _               -> throwError $ err500
 
 runStorage :: (Member IO m) => MVar Store -> Eff (StorageEffect : m) x -> Eff m x
 runStorage store' = interpret eval where
   eval :: (Member IO n) => StorageEffect a -> Eff n a
   eval (AddDocument key bytes) = send $ modifyMVar store' $
-    \Store{collections} -> pure (Store{collections = Map.update (\c -> Just $ bytes : c) key collections}, () )
+    \Store{collections} ->
+      let
+        existing = maybe [] id $ Map.lookup key collections
+        updated = bytes : existing
+      in pure ( Store{collections = Map.insert key updated collections}, Right updated )
 
 type PetStoreSignature = AddPetRoute
 
-type AddPetRoute = "pets"  :> ReqBody '[JSON] Pet :> Post '[JSON] String -- keep/
+type AddPetRoute = "pets"  :> ReqBody '[JSON] Pet :> Post '[JSON] [Pet] -- keep/
 
 data Pet = Pet{
   petName :: String
-} deriving (Generic, FromJSON, ToJSON)
+} deriving (Generic, FromJSON, ToJSON, Show, Eq)
 
 data BusinessLogicEffect a where
-  AddPet :: Pet -> BusinessLogicEffect String
+  AddPet :: Pet -> BusinessLogicEffect [Pet]
 
 data StorageEffect a where
-  AddDocument :: Key -> ByteString -> StorageEffect ()
+  AddDocument :: Key -> ByteString -> StorageEffect (Either String [ByteString])
 
 type Key = String
 
--- pet-store-backend/test/PetStoreSpec.hs
+++ pet-store-backend/test/PetStoreSpec.hs
@@ -1,17 +1,21 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 {-# OPTIONS_GHC -Wno-unused-matches #-}
 
 module PetStoreSpec where
 
-import           Control.Concurrent.MVar (MVar, modifyMVar, newMVar)
-import           Control.Monad.Freer     (Eff, Member, interpret, runM, send)
-import qualified Data.Map                as Map
+import           Control.Concurrent.MVar   (MVar, modifyMVar, newMVar)
+import           Control.Monad.Freer       (Eff, Member, interpret, runM, send)
+import           Control.Monad.Freer.Error (runError)
+import qualified Data.Map                  as Map
 import           PetStore.Server
 import           Test.Hspec
+import           Servant
 
 spec :: Spec
 spec = describe "pet store tests" $ do
   it "add pet" $ do
     store <- newMVar $ Store Map.empty
-    actual <- runM $ runStorage store $ runBusinessLogic  $ send $ AddPet $ Pet "test-pet"
-    actual `shouldBe` "got: test-pet"
+    let testPet = Pet "test-pet"
+    actual :: Either ServantErr [Pet] <- runM $ runError $ runStorage store $ runBusinessLogic $ send $ AddPet $ testPet
+    actual `shouldBe` (Right [testPet])
