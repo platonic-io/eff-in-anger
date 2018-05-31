7e40156eacd00f9db0a963d5f8505fc8d5662626
Christopher Vogt <oss.nsp@cvogt.org>
2018-05-31 14:17:22 -0400

    delete unused files

--- pet-store-payment/src/main/java/aleryo/PaymentConfiguration.java
+++
@@ -1,9 +0,0 @@
-package aleryo;
-
-import io.dropwizard.Configuration;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
-import javax.validation.constraints.*;
-
-public class PaymentConfiguration extends Configuration {
-}
